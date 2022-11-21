1510a08f02c9b545441d45fc45a0bbd536212e96
AshKatzEm <92552105+AshKatzEm@users.noreply.github.com>
2022-11-21 21:24:06 +0300

    Update arithmetic_formatter.py

--- arithmetic_formatter.py
+++ arithmetic_formatter.py
@@ -1,71 +1,67 @@
-import re  # Import regex for string parsing.
+def arithmetic_arranger(problems, solve=False): #The function takes an optional second argument. When set to True, the answers are displayed in the final arrangment.
 
-# Start of function definition. "solve" is an optional parameter that defaults to False; if True, it causes the answer for each calculation to be printed under the dashed line.
-def arithmetic_arranger(problems, solve=False):
-
-    # Check for number of problems in the string.
+    # Check the number of problems in the list
     if len(problems) > 5:
         return "Error: Too many problems."
 
-    # Variable Initialisation
-    top_line = ""
-    bottom_line = ""
-    dash_line = ""
-    result_line = ""
+    # Initialize a string for each component of the problems
+    numerators = ""
+    denominators = ""
+    equalsLines = ""
+    results = ""
 
-    # Iterate through the problem strings in the problems list.
+
+    # Iterate through the problems
     for problem in problems:
-        # Check string only contains digits or the + or - operators.
-        if re.search("[^\s\d.+-]", problem):
-            if re.search("[/*]", problem):
+        # Check that the problem only contains digits or the + or - operators
+        if problem.replace(" ", "").isdigit():
+            if '/' in problem or '*' in problem:
                 return "Error: Operator must be '+' or '-'."
             else:
                 return "Error: Numbers must only contain digits."
 
-        # Parse the problems list and obtain the two numbers and the operator from each string.
+        # Split the problem up and save the two numbers and the operator
         first_number = problem.split()[0]
         operator = problem.split()[1]
         second_number = problem.split()[2]
 
-        # Check that each number does not have more than four digits.
+        # Check that each number does not have more than four digits
         if len(first_number) > 4 or len(second_number) > 4:
             return "Error: Numbers cannot be more than four digits."
 
-        # Calculate the sum of the numbers from each problem.
-        sum = ""
+        # Define problem_width as the widest of the numbers plus the operator plus a space
+        problem_width = max(len(first_number), len(second_number)) + 2
+        solution = ""
+        equalsLine = ""
+        # Justify the numbers to the right according to the problem_width
+        numerator= str(first_number).rjust(problem_width)
+        denominator = operator + str(second_number).rjust(problem_width - 1)
+        # Calculate the solution of each problem
         if operator == "+":
-            sum = int(first_number) + int(second_number)
+            solution = int(first_number) + int(second_number)
         elif operator == "-":
-            sum = int(first_number) - int(second_number)
+            solution = int(first_number) - int(second_number)
+        result = str(solution).rjust(problem_width)
+      # equalsLine is proportional to problem_width
+        for i in range(problem_width):
+            equalsLine += "-"
 
-        # Define the line length as the maximum length of the biggest number out of the two, plus two characters (operator and a whitespace).
-        line_length = max(len(first_number), len(second_number)) + 2
-        # Justify the numbers to the right according to the respective line length.
-        top = str(first_number).rjust(line_length)
-        bottom = operator + str(second_number).rjust(line_length - 1)
-        result = str(sum).rjust(line_length)
-        # Create a dashed line with a number of dashes equal to the line length.
-        dashes = ""
-        for i in range(line_length):
-            dashes += "-"
-
-        # If problem is not the last one in the list, append the justified numbers to the displayed lines and add 4 spaces for the next problem.
+        # If the problem is not the last one in the list, append the formatted parts of the problem and 4 spaces for the next problem
         if problem != problems[-1]:
-            top_line += top + "    "
-            bottom_line += bottom + "    "
-            dash_line += dashes + "    "
-            result_line += result + "    "
-        # If the problem is the last one in the list, do not add spaces.
+            numerators += numerator+ "    "
+            denominators += denominator + "    "
+            equalsLines += equalsLine + "    "
+            results += result + "    "
+        # If it is, don't add the 4 spaces
         else:
-            top_line += top
-            bottom_line += bottom
-            dash_line += dashes
-            result_line += result
+            numerators += numerator
+            denominators += denominator
+            equalsLines += equalsLine
+            results += result
 
     # Arrange the lines with newlines to separate each string.
     if solve is True:
-        arranged = top_line + "\n" + bottom_line + "\n" + dash_line + "\n" + result_line
+        arranged = numerators + "\n" + denominators + "\n" + equalsLines + "\n" + results
     else:
-        arranged = top_line + "\n" + bottom_line + "\n" + dash_line
-    # print(arranged)
+        arranged = numerators + "\n" + denominators + "\n" + equalsLines
     return arranged
